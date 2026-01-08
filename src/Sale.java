9ef7e6ddd5893ef744cfc839282407f8bb8ef685
magzhanseitkapar <250606@astanait.edu.kz>
2026-01-08 12:56:37 +0500

    Maga Top

---
+++ src/Sale.java
@@ -0,0 +1,31 @@
+public class Sale {
+    private Product product;
+    private Customer customer;
+    private int quantity;
+
+    public Sale(Product product, Customer customer, int quantity) {
+        this.product = product;
+        this.customer = customer;
+        this.quantity = quantity;
+    }
+
+    public void completeSale() {
+        double totalPrice = product.getPrice() * quantity;
+
+        if (!customer.canPay(totalPrice)) {
+            System.out.println("Not enough balance!");
+            return;
+        }
+
+        if (product.getQuantity() < quantity) {
+            System.out.println("Not enough product in stock!");
+            return;
+        }
+
+        customer.pay(totalPrice);
+        product.setQuantity(product.getQuantity() - quantity);
+
+        System.out.println("Sale completed!");
+    }
+}
+
